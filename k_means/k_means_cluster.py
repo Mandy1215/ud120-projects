3640527b2fadc2233a4b4ace340c5b3431d52d21
Mandy <1113034387@qq.com>
2017-12-18 19:14:32 +0800

    kmeans

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python 
-
+# -*- coding: utf-8 -*-
 """ 
     Skeleton code for k-means clustering mini-project.
 """
@@ -16,7 +16,8 @@ from feature_format import featureFormat, targetFeatureSplit
 
 
 
-
+# 画出散点图
+##enumerate(sequence, [start=0])：python的内置 函数。用于将一个可遍历的数据对象(如列表、元组或字符串)组合为一个索引序列，同时列出数据和数据下标
 def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature 1", f2_name="feature 2"):
     """ some plotting code designed to help you visualize your clusters """
 
@@ -37,19 +38,20 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
     plt.show()
 
 
-
+#pickle.load(file)：反序列化对象。将文件中的数据解析为一个Python对象。
 ### load in the dict of dicts containing all the data on each person in the dataset
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 ### there's an outlier--remove it! 
-data_dict.pop("TOTAL", 0)
+data_dict.pop("TOTAL", 0)  #删除离群值
 
 
 ### the input features we want to use 
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2 , feature_3 ]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -58,14 +60,25 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
+for f1, f2 ,f3 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
+##缩放
+from sklearn.preprocessing import MinMaxScaler
+def scaler(feature_train):
+    MMS = MinMaxScaler()
+    a = MMS.fit_transform(feature_train)
+    return a
+
+
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-
-
+##聚类
+from sklearn.cluster import KMeans
+#pred = KMeans(n_clusters=2 ).fit_predict(finance_features)  #部署聚类：创建拟合器 然后fit 然后predict
+kmeans = KMeans(n_clusters=2 ).fit(scaler(finance_features))
+pred = kmeans.labels_
 
 
 ### rename the "name" parameter when you change the number of features
@@ -74,3 +87,22 @@ try:
     Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
+
+
+
+##计算薪酬范围
+array1 = numpy.array(finance_features)
+m_f1 = max(array1[:,0])
+print("max salary:",m_f1)
+for f1,f2,f3 in array1:
+    if f1 > 0:
+        m_f1 = min(f1, m_f1)
+print("min salary:",m_f1)
+##计算股权范围
+array2 = numpy.array(finance_features)
+m_f2 = max(array2[:,1])
+print("max exercised_stock_options:" , m_f2)
+for f1,f2, f3 in array2:
+    if f2>0:
+        m_f2 = min(f2, m_f2)
+print("min exercised_stock_options:" , m_f2)
